ac7610f5571848c88c39e1b0bbbc00f85dd0ac97
Shadan Kazmi <125872053+ShadanKazmi@users.noreply.github.com>
2024-08-25 20:06:27 +0530

    Update TeamsContext.jsx

--- frontend/src/api/TeamsContext.jsx
+++ frontend/src/api/TeamsContext.jsx
@@ -1,33 +1,3 @@
-// import { createContext, useEffect, useState } from "react";
-// import Cookies from 'js-cookie';
-// import axios from "axios";
-
-// export const teamContext = createContext(null);
-
-// export const TeamProvider = (props) => {
-//     const [teams, setTeams] = useState([]);
-
-//     useEffect(() => {
-//         const userId = localStorage.getItem('userId'); 
-//         const fetchTeams = async () => {
-//             try {
-//                 const response = await axios.get(`https://collab-app-backend.onrender.com/team/userTeams/${userId}`);
-//                 setTeams(response.data);
-//             } catch (error) {
-//                 console.error('Error fetching teams:', error);
-//             }
-//         };
-
-//         fetchTeams();
-//     }, []);
-
-//     return (
-//         <teamContext.Provider value={{teams, setTeams}}>
-//             {props.children}
-//         </teamContext.Provider>
-//     );
-// };
-
 import { createContext, useEffect, useState } from "react";
 import Cookies from 'js-cookie';
 import axios from "axios";
@@ -38,7 +8,7 @@ export const TeamProvider = (props) => {
     const [teams, setTeams] = useState([]);
 
     useEffect(() => {
-        const userId = Cookies.get('userId'); // Fetch userId from cookie
+        const userId = Cookies.get('userId');
 
         const fetchTeams = async () => {
             try {
