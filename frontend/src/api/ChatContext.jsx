2e28fa27ed0242c80a1e956bcd925d85b440a22c
Shadan Kazmi <125872053+ShadanKazmi@users.noreply.github.com>
2024-08-25 20:06:08 +0530

    Update ChatContext.jsx

--- frontend/src/api/ChatContext.jsx
+++ frontend/src/api/ChatContext.jsx
@@ -1,23 +1,3 @@
-// import React, { createContext, useState } from 'react';
-
-// export const MessageContext = createContext();
-
-// export const MessageProvider = ({ children }) => {
-//   const [selectedUser, setSelectedUser] = useState(null); // To store the selected user for messaging
-//   const [chats, setChats] = useState([]); // Array to store chats
-
-//   const addChat = (chat) => {
-//     setChats([...chats, chat]);
-//   };
-
-
-//   return (
-//     <MessageContext.Provider value={{ selectedUser, setSelectedUser,chats, addChat  }}>
-//       {children}
-//     </MessageContext.Provider>
-//   );
-// };
-
 import React, { createContext, useState, useEffect } from 'react';
 import Cookies from 'js-cookie';
 
@@ -27,11 +7,10 @@ export const MessageProvider = ({ children }) => {
   const [selectedUser, setSelectedUser] = useState(null);
   const [chats, setChats] = useState([]);
 
-  // Function to add a chat to the state and cookies
   const addChat = (chat) => {
     const updatedChats = [...chats, chat];
     setChats(updatedChats);
-    Cookies.set('chats', JSON.stringify(updatedChats), { expires: 7 }); // Set cookie with 7 days expiration
+    Cookies.set('chats', JSON.stringify(updatedChats), { expires: 7 });
   };
 
   const updateChatSubtitle = (userId, newSubtitle) => {
@@ -42,12 +21,11 @@ export const MessageProvider = ({ children }) => {
 
 
   useEffect(() => {
-    // Retrieve chats from cookies on component mount
     const storedChats = Cookies.get('chats');
     if (storedChats) {
       setChats(JSON.parse(storedChats));
     }
-  }, []); // Empty dependency array ensures this effect runs only once on mount
+  }, []);
 
   return (
     <MessageContext.Provider value={{ selectedUser, setSelectedUser, chats, addChat }}>
